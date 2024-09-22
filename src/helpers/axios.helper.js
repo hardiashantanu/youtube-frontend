db2a74cbedb3dded28198c0641766b72749a9fe5
hardiashantanu <127988175+hardiashantanu@users.noreply.github.com>
2024-09-22 14:52:01 +0530

    initial commit

---
+++ src/helpers/axios.helper.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+// import { loadProgressBar } from "axios-progress-bar";
+// import "axios-progress-bar/dist/nprogress.css";
+
+// const baseURL = "https://playtubebyyashpz.onrender.com/api/v1";
+const baseURL = "https://videotube-7ue6.onrender.com/api/v1";
+// const baseURL = "http://localhost:9000/api/v1";
+
+export const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+// loadProgressBar({}, axiosInstance);
